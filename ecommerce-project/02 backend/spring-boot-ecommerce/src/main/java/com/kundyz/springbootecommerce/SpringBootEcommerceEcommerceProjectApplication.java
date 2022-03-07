5c9bff586f89e286a92d12dfcf13031a40cca39a
Hongduzi Saiershanbayi <92166985+Kikoonam@users.noreply.github.com>
2022-03-07 18:41:43 +0100

    spring boot initilizer added

---
+++ ecommerce-project/02 backend/spring-boot-ecommerce/src/main/java/com/kundyz/springbootecommerce/SpringBootEcommerceEcommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kundyz.springbootecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceEcommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceEcommerceProjectApplication.class, args);
+	}
+
+}
---
+++ ecommerce-project/02 backend/spring-boot-ecommerce/src/test/java/com/kundyz/springbootecommerce/SpringBootEcommerceEcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kundyz.springbootecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceEcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
